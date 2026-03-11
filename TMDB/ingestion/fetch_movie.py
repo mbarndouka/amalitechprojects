d82ce07368ed582eafc4e588e4da725a5d655be3
mbarndouka <mmbarndouka@gmail.com>
2026-03-11 14:58:58 +0200

    Add functionality to save fetched movie data to parquet format

--- TMDB/ingestion/fetch_movie.py
+++ TMDB/ingestion/fetch_movie.py
@@ -9,6 +9,8 @@ token = os.getenv("access_token")
 
 movie_ids = [0, 299534, 19995, 140607, 299536, 597, 135397, 420818, 24428, 168259, 99861, 284054, 12445, 181808, 330457, 351286, 109445, 321612, 260513]
 
+RAW_DATA_PATH = "data/raw/raw_movies.parquet"
+
 def fetch_movie():
     client = TMDB(token)
     movies = []
@@ -22,4 +24,7 @@ def fetch_movie():
             print(f"Error fetching movie with ID {movie_id}: {e}")
     
     df = pd.DataFrame(movies)
+    os.makedirs(os.path.dirname(RAW_DATA_PATH), exist_ok=True)
+    # save dataFrame
+    df.to_parquet(RAW_DATA_PATH, index=False)
     return df
